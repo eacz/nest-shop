f250d9f6615e5f99c1d0e0979c27cdb6a766a52b
Esteban Canteros <estebanacanteros@gmail.com>
2022-12-15 12:10:21 -0300

    add beforeUpdate

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,10 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Product {
@@ -39,5 +45,11 @@ export class Product {
       .replaceAll("'", '');
   }
 
-  //@BeforeUpdate()
+  @BeforeUpdate()
+  checkSlugBeforeUpdate() {
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
 }
