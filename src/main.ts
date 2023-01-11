7b61cedfdac65a4834be07c4c474e642def1c73c
Esteban Canteros <estebanacanteros@gmail.com>
2023-01-11 18:57:36 -0300

    initial setup for swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +10,16 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo Restful API')
+    .setDescription('Teslo Shop Endpoints')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 4000);
   console.log(`App runnning on port ${process.env.PORT}`);
 }
