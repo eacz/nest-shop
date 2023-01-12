eca56ecd4494a4d3b34cb1fb940c62a972030683
Esteban Canteros <estebanacanteros@gmail.com>
2023-01-12 08:50:08 -0300

    expanding api properties documentation

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -13,39 +13,86 @@ import { ProductImage } from './product-image.entity';
 
 @Entity({ name: 'products' })
 export class Product {
-  @ApiProperty()
+  @ApiProperty({
+    example: '0328ecb1-0c61-4594-b5d6-bcb48cee6df0',
+    description: 'Product Id',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
-  @ApiProperty()
+
+  @ApiProperty({
+    example: 'T-shirt',
+    description: 'Product Title',
+    uniqueItems: true,
+  })
   @Column('text', { unique: true })
   title: string;
-  @ApiProperty()
+
+  @ApiProperty({
+    default: 0,
+    example: 120,
+    description: 'Product Price',
+  })
   @Column('float', { default: 0 })
   price: number;
-  @ApiProperty()
+
+  @ApiProperty({
+    default: null,
+    description: 'Product description',
+    example: 'lorem ipsum',
+  })
   @Column({ type: 'text', nullable: true })
   description: string;
-  @ApiProperty()
+
+  @ApiProperty({
+    example: 't_shirt',
+    description: 'slug for CEO',
+    uniqueItems: true,
+  })
   @Column('text', { unique: true })
   slug: string;
-  @ApiProperty()
+
+  @ApiProperty({
+    example: 12,
+    description: 'Product Sizes',
+  })
   @Column('int', { default: 0 })
   stock: number;
-  @ApiProperty()
+
+  @ApiProperty({
+    example: ['M', 'L', 'XL'],
+    default: 0,
+    description: 'Product current stock',
+  })
   @Column('text', { array: true })
   sizes: string[];
-  @ApiProperty()
+
+  @ApiProperty({
+    example: 'women',
+    description: 'Product Genre',
+  })
   @Column('text')
   gender: string;
-  @ApiProperty()
+
+  @ApiProperty({
+    example: ['t-shirt', 'black'],
+    description: 'Product tags for quick search or for group products',
+  })
   @Column('text', { array: true, default: [] })
   tags: string[];
-  @ApiProperty()
+
+  @ApiProperty({
+    example: ['https://eacz.com/image1.jpg', 'https://eacz.com/image2.jpg'],
+    description: 'Product Images',
+    default: [],
+  })
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true,
   })
   images?: ProductImage[];
+
   @ManyToOne(() => User, (user) => user.products, { eager: true })
   user: User;
 
