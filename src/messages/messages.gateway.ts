5e69381b8c71b62a36b49c63e74dbcbe1cb296b1
Esteban Canteros <estebanacanteros@gmail.com>
2023-01-14 06:56:17 -0300

    client connect and disconnect

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,7 +1,22 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+
 import { MessagesService } from './messages.service';
 
 @WebSocketGateway({ cors: true })
-export class MessagesGateway {
+export class MessagesGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messagesService: MessagesService) {}
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
 }
