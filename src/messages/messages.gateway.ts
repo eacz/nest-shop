98ceda2aab1f85433eb79a7d6e7fa5db931fad2e
Esteban Canteros <estebanacanteros@gmail.com>
2023-01-16 11:10:25 -0300

    emit messages event

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,10 +1,12 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 import { MessagesService } from './messages.service';
 
@@ -28,5 +30,26 @@ export class MessagesGateway
       'clients-updated',
       this.messagesService.getConnectedClients(),
     );
+  }
+
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto) {
+    //emit just to the client
+    //client.emit('message-from-server', {
+    //  fullName: 'Pedro',
+    //  message: payload.message,
+    //});
+
+    //emit to everyone except client
+    //client.broadcast.emit('message-from-server', {
+    //  fullName: 'Pedro',
+    //  message: payload.message,
+    //});
+
+    //emit to everyone (including client)
+    this.wss.emit('message-from-server', {
+      fullName: 'Pedro',
+      message: payload.message,
+    });
   }
 }
